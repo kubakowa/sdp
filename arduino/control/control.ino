ca8abb919872cd3382b3383b3dc8a63c6718311b
Kuba Kowalski <j.kowalski.wroclaw@gmail.com>
2015-03-01 12:20:27 +0000

    init the motor speeds

--- arduino/control/control.ino
+++ arduino/control/control.ino
@@ -10,14 +10,14 @@
 
 /* State */
 int GRABBER_OPEN = 0;
-int ackNo = 0;
 
 /* Constants */
 int KICK_TIME = 400;
 int GRAB_TIME = 200;
 int CLOSE_TIME = 250;
 int MAX_SPEED = 100;
-int OPEN_SPEED = 45;
+int OPEN_SPEED = 40;
+int INIT_TIME = 50;
 
 // command
 SerialCommand comm;
@@ -30,7 +30,7 @@ void setupCommands() {
   comm.addCommand("BB_STOP", make_stop);
   comm.addCommand("BB_PAUSE", make_pause);
   comm.addCommand("BB_STEP", make_incremental_move);
-  comm.addCommand("BB_FLUSH", make_flush);
+  comm.addCommand("BB_RELEASE", release_grabber);
 } 
 
 void setup(){
@@ -46,28 +46,41 @@ void burst_move(int left_speed,int right_speed, int back_speed) {
     motorStop(BACK_MOTOR);
     
   if (left_speed > 0)
+    motorForward(LEFT_MOTOR, MAX_SPEED);
+    delay(INIT_TIME);
     motorForward(LEFT_MOTOR, left_speed);
   else if (left_speed < 0)
+    motorBackward(LEFT_MOTOR, -MAX_SPEED);
+    delay(INIT_TIME);
     motorBackward(LEFT_MOTOR, -left_speed);
     
   if (right_speed > 0)
+    motorForward(RIGHT_MOTOR, MAX_SPEED);
+    delay(INIT_TIME);
     motorForward(RIGHT_MOTOR, right_speed);
   else if (right_speed < 0)
+    motorBackward(RIGHT_MOTOR, -MAX_SPEED);
+    delay(INIT_TIME);
     motorBackward(RIGHT_MOTOR, -right_speed);
   
   if (forward)
     return;
       
   if (back_speed > 0)
+    motorForward(BACK_MOTOR, MAX_SPEED);
+    delay(INIT_TIME);
     motorForward(BACK_MOTOR, back_speed);
   else if (back_speed < 0)  
+    motorBackward(BACK_MOTOR, -MAX_SPEED);
+    delay(INIT_TIME);
     motorBackward(BACK_MOTOR, -back_speed); 
 }
  
 void make_incremental_move() {
   char *left_m;
   char *right_m;
-  char *back_m; 
+  char *back_m;
+  
   int left_speed;
   int right_speed;
   int back_speed;
@@ -109,87 +122,45 @@ void make_move() {
   burst_move(left_speed, right_speed, back_speed);
 }
 
-// Volatile Commands
-bool verify_command(char* command){
-  // Get broadcast acknowledgement number
+void make_kick() {
   
+  if (GRABBER_OPEN)
+    return;
   
-  int ack_number_pc;
-  ack_number_pc = atoi(comm.next());
-  
-  // Check for duplicates - commands received in the past
-  if (ack_number_pc != ackNo){
-    // Send the acknowledgement again
-    String ack = command;
-    ack += " ";
-    ack += ack_number_pc;
-    ack += "\n";
-    
-    Serial.print(ack);
-    return false;
-  }
-  
-  // If fresh, verify
-  else {
-    return true; 
-  }
-}
-
-void send_ack(char* command){
-   // Send acknowledgement
-   String ack = command;
-    ack += " ";
-    ack += ackNo;
-    ack += "\n";
-   Serial.print("hello this is a message that you shouldn't corrupted\n"); 
-   Serial.print(ack);
-   ackNo++;
-}
-
-void make_flush(){
-  Serial.flush();
-}
-
-void make_kick() { 
-  if(verify_command("BB_KICKED")){
-    if (GRABBER_OPEN)
-      return;
-    
-    motorBackward(KICK_MOTOR, MAX_SPEED);
-    delay(KICK_TIME);
-    motorStop(KICK_MOTOR);
-
-    send_ack("BB_KICKED");
-  }
+  motorBackward(KICK_MOTOR, MAX_SPEED);
+  delay(KICK_TIME);
+  motorStop(KICK_MOTOR);
+  Serial.print("BB_KICKED \n");
 }
 
 void open_grabber() {
-  if(verify_command("BB_OPENED")){ 
-    if (GRABBER_OPEN)
-      return;
-    
-    
-    motorBackward(KICK_MOTOR, MAX_SPEED);
-    delay(GRAB_TIME);
-    motorBackward(KICK_MOTOR, OPEN_SPEED);
-    
-    /* Assign state of the grabber */
-    GRABBER_OPEN = 1;
-    send_ack("BB_OPENED");
-  }
+  
+  if (GRABBER_OPEN)
+    return;
+  
+  motorBackward(KICK_MOTOR, MAX_SPEED);
+  delay(GRAB_TIME);
+  motorBackward(KICK_MOTOR, OPEN_SPEED);
+  
+  /* Assign state of the grabber */
+  GRABBER_OPEN = 1;
+  Serial.print("BB_OPENED \n");
 }
 
 void close_grabber() {
-  if(verify_command("BB_CLOSED")){
-    motorStop(KICK_MOTOR);
-    motorForward(KICK_MOTOR, MAX_SPEED);
-    delay(CLOSE_TIME);
-    motorStop(KICK_MOTOR);
-    
-    /* Assign state of the grabber */
-    GRABBER_OPEN = 0;
-    send_ack("BB_CLOSED");
-  } 
+  motorStop(KICK_MOTOR);
+  motorForward(KICK_MOTOR, MAX_SPEED);
+  delay(CLOSE_TIME);
+  motorForward(KICK_MOTOR, OPEN_SPEED);;
+  
+  /* Assign state of the grabber */
+  GRABBER_OPEN = 0;
+  Serial.print("BB_CLOSED \n");
+}
+
+void release_grabber() {
+  motorStop(KICK_MOTOR);
+  GRABBER_OPEN = 0;
 }
 
 void make_pause() {
@@ -200,12 +171,10 @@ void make_pause() {
 }
 
 void make_stop() {
-  if(verify_command("BB_STOPPED")){
-    motorStop(BACK_MOTOR);
-    motorStop(LEFT_MOTOR); 
-    motorStop(RIGHT_MOTOR);
-    send_ack("BB_STOPPED");
-  }
+  motorStop(BACK_MOTOR);
+  motorStop(LEFT_MOTOR); 
+  motorStop(RIGHT_MOTOR);
+  Serial.print("BB_STOPPED \n");
 }
 
 void invalid_command(const char* command) { }
